f8d0fe58e7c66c03655afeb0b6363020e18201a3
xtell <evstafyevan96@gmail.com>
2020-07-27 20:39:41 +0500

    update

--- api/index.js
+++ api/index.js
@@ -1,1 +1,3 @@
 export {getUserToken } from './methods.js'
+export {getUserId} from './methods.js'
+export {getUserInfo} from './methods.js'
--- api/methods.js
+++ api/methods.js
@@ -1,4 +1,4 @@
-async function makeRequest(method, endPoint, headers = {}, data = {}) {
+async function makeRequest(method, endPoint, headers = {}, data = null) {
   const baseUrl = "https://waleraspider.ml/api/";
   let response = await fetch(baseUrl + endPoint, {
     method: method,
@@ -6,15 +6,16 @@ async function makeRequest(method, endPoint, headers = {}, data = {}) {
       "Content-Type": "application/json",
       ...headers,
     },
-    body: JSON.stringify(data),
+
+    body: data ? JSON.stringify(data) : undefined,
   });
-  return response
-    // .then((response) => {
-    //   return response;
-    // })
-    // .catch((error) => {
-    //   return error;
-    // });
+  return response;
+  // .then((response) => {
+  //   return response;
+  // })
+  // .catch((error) => {
+  //   return error;
+  // });
 }
 
 export async function getUserToken(nickName, password) {
@@ -22,12 +23,70 @@ export async function getUserToken(nickName, password) {
     NickName: nickName,
     Password: password,
   };
-  return await makeRequest("POST", "Authentication/token", {}, data)
-    // .then((response) => {
-    //   return response;
-    // })
-    // .catch((error) => {
-    //   return error;
-    // });
+  return await makeRequest("POST", "Authentication/token", {}, data);
+  // .then((response) => {
+  //   return response;
+  // })
+  // .catch((error) => {
+  //   return error;
+  // });
 }
-export async function createUser()
+export async function getUserId(token) {
+  return await makeRequest("GET", "Users/current", {
+    Authorization: "Bearer " + token,
+  });
+}
+export async function getAllUsers(token) {
+  return await makeRequest("GET", "Users", {
+    Authorization: "Bearer " + token,
+  });
+}
+export async function getUserInfo(token, userId) {
+  return await makeRequest("GET", `Users/${userId}`, {
+    Authorization: "Bearer " + token,
+  });
+}
+export async function getCurrentUserFriends(token) {
+  return await makeRequest("GET", "Users/friends", {
+    Authorization: "Bearer " + token,
+  });
+}
+export async function getUserFriends(token, userId) {
+  return await makeRequest("GET", "Users/friends", {
+    Authorization: "Bearer " + token,
+  });
+}
+export async function addFriend(token, userId, friendId) {
+  const data = {
+    UserId: userId,
+    FriendId: friendId
+  };
+  return await makeRequest("POST", "Users/add_friend", {
+    Authorization: "Bearer " + token, 
+  }, data);
+}
+export async function deleteFriend(token, userId, friendId) {
+  const data = {
+    UserId: userId,
+    FriendId: friendId
+  };
+  return await makeRequest("DELETE", "Users/delete_friend", {
+    Authorization: "Bearer " + token, 
+  }, data);
+}
+export async function createUser(name, lastName, nickName, password) {
+  const data = {
+    Name: name,
+    LastName: lastName,
+    Nickname: nickName,
+    Password: password
+  };
+  return await makeRequest("POST", "Users/create", {
+    Authorization: "Bearer " + token
+  }, data);
+}
+export async function updateUser(data) {
+  return await makeRequest("POST", "Users/update", {
+    Authorization: "Bearer " + token
+  }, data);
+}
