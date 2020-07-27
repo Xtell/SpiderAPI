8d0f87ef9efa1f6a27defe382bb1539c3b486b1f
xtell <evstafyevan96@gmail.com>
2020-07-27 19:48:30 +0500

    Add getUserToken

---
+++ api/makeRequest.js
@@ -0,0 +1,32 @@
+async function makeRequest(method, endPoint, headers = {}, data = {}) {
+  const baseUrl = "https://waleraspider.ml/api/";
+  let response = await fetch(baseUrl + endPoint, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+    body: JSON.stringify(data),
+  });
+  return response
+    // .then((response) => {
+    //   return response;
+    // })
+    // .catch((error) => {
+    //   return error;
+    // });
+}
+
+export async function getUserToken(nickName, password) {
+  let data = {
+    NickName: nickName,
+    Password: password,
+  };
+  return await makeRequest("POST", "Authentication/token", {}, data)
+    // .then((response) => {
+    //   return response;
+    // })
+    // .catch((error) => {
+    //   return error;
+    // });
+}
